3b9601305908814669c6e7a7c15d026446214a0c
SekaiNoAwari <lchlibra@gmail.com>
2023-07-14 10:41:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '60acdab1201a4b6fb198c37b08cd86ca',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pGHdaBbzPle_iJgVertoHGLyPo1tN0Ihd87x2jUSqXQ',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-19',
       festivals: [
