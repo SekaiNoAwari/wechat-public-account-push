29549dc663fcf99d8392e499edd813f26bd0ca8c
SekaiNoAwari <lchlibra@gmail.com>
2023-07-17 09:53:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -10,10 +10,10 @@ const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
   // USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx9c5cd56d423d7e92',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '5e3afedcfbb85e07ee0f7663557d4137',
 
   PROVINCE: '福建',
   CITY: '南平',
@@ -46,7 +46,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oM0O66AldeNozyV5drFRtoCymUew',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'fVAlMkwAGPsgXWkux-My8q1PgwOK-cQUdgtKv7irYZk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-19',
       festivals: [
@@ -68,7 +68,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: 'ip0siryi0jsSbnxve8wFvNQTKtFlEdcUaShzislVx2k',
 
   CALLBACK_USERS: [
     {
