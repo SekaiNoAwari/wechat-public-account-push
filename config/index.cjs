57f2cd5d090c7f726311d144526040e7564f92da
SekaiNoAwari <lchlibra@gmail.com>
2023-07-17 16:31:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -46,7 +46,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oM0O66PgZod7I_BumEHn_VHJsG2I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'azr7ysQfJ8GqS8bFcHHURJz5WdpwcdkhIfERnKTEYhw',
+      useTemplateId: 'P4F0q8hJFhRG0t1_3Rhy0HHez8KLz4N7KhqilwMv4Go',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-19',
       festivals: [
@@ -73,7 +73,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oM0O66AldeNozyV5drFRtoCymUew',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'azr7ysQfJ8GqS8bFcHHURJz5WdpwcdkhIfERnKTEYhw',
+      useTemplateId: 'P4F0q8hJFhRG0t1_3Rhy0HHez8KLz4N7KhqilwMv4Go',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-06',
       festivals: [
